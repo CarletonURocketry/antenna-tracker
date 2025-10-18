fc71bbe127b444bbc3c634c865274861fe54f40c
Jia Lin <99093620+justapotato213@users.noreply.github.com>
2025-10-18 11:16:07 -0400

    fix merge stuff

--- tracker/src/tracker.c
+++ tracker/src/tracker.c
@@ -1,40 +1,29 @@
-#include "packets/packet.h"
-#include <errno.h>
+#include "tracker.h"
+#include <pthread.h>
+#include "collection/collection.h"
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
-int parse_packet(char *buffer) {
-    pkt_hdr_t *header = (pkt_hdr_t *)buffer;
-    /* There was a bug where instead of padding the call sign with null terminators we just padded wiht ascii 0's,
-       as a workaround for now we can assume the call sign len is always 6 chars
-    */
-    printf("Call sign: %.6s\n", header->call_sign);
+int main(void){
 
-    return 0;
-}
+    int err;
+    err = setup_syslogging();
+        if(err != 0){
+            printf("Failed to setup syslogging: %s\n", strerror(err));
+            return -1;
+        }
 
-int main(void) {
-    FILE *telem_file = fopen("./mocking/old-el-blasto/el_blasto.hex", "r");
-    if (telem_file == NULL) {
-        printf("Failed to open telem file: %s\n", strerror(errno));
+    pthread_t collection_thread;
+
+    ininfo("Starting tracker\n");
+    err = pthread_create(&collection_thread, NULL, collection_main, NULL);
+    if(err != 0){
+        inerr("Failed to create collection thread: %s\n", strerror(err));
         return -1;
     }
 
-    char buffer[PACKET_MAX_SIZE];
-    char line[PACKET_MAX_SIZE * 2];
-    while (fgets(line, sizeof(line), telem_file) != NULL) {
-        size_t byte_count = 0;
-
-        /* TODO: handle odd len hex strings*/
-        for (size_t i = 0; i < strlen(line); i += 2) {
-            if (line[i] == '\n' || line[i + 1] == '\n') {
-                break;
-            }
-
-            sscanf(&line[i], "%2hhx", (unsigned char *)&buffer[byte_count++]);
-        }
-        parse_packet(buffer);
-    }
+    pthread_join(collection_thread, NULL);
 
     return 0;
 }
