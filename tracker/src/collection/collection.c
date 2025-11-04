fed8f04379dc05bc92f45a4daf0e0273bf31535e
Daniel Byshkin <daniel.byshkin@gmail.com>
2025-11-03 20:57:00 -0500

    Reading and printing data from radio

--- tracker/src/collection/collection.c
+++ tracker/src/collection/collection.c
@@ -6,6 +6,50 @@
 #include <pthread.h>
 #include "radio.h"
 #include "mocking/el_blasto.h"
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#ifdef CONFIG_LPWAN_RN2XX3
+    #include <nuttx/wireless/ioctl.h>
+    #include <nuttx/wireless/lpwan/rn2xx3.h>
+#endif
+
+#define config_error(err)                                                                                              \
+    if (err) {                                                                                                         \
+        err = errno;                                                                                                   \
+        inerr("Error configuring radio, line %d: %d\n", __LINE__, err);                                                \
+        return err;                                                                                                    \
+    }
+    
+int configure_radio(int fd, struct radio_options const *config) {
+    int err = 0;
+
+// #if defined(CONFIG_LPWAN_RN2XX3)
+    int32_t txpwr = config->txpwr * 100;
+    uint64_t sync = config->sync;
+
+    err = ioctl(fd, WLIOC_SETRADIOFREQ, config->freq);
+    config_error(err);
+    err = ioctl(fd, WLIOC_SETTXPOWERF, &txpwr);
+    config_error(err);
+    err = ioctl(fd, WLIOC_SETSPREAD, config->spread);
+    config_error(err);
+    err = ioctl(fd, WLIOC_SETCODERATE, config->cr);
+    config_error(err);
+    err = ioctl(fd, WLIOC_SETBANDWIDTH, config->bw);
+    config_error(err);
+    err = ioctl(fd, WLIOC_CRCEN, config->crc);
+    config_error(err);
+    err = ioctl(fd, WLIOC_IQIEN, config->iqi);
+    config_error(err);
+    err = ioctl(fd, WLIOC_SETSYNC, &sync);
+    config_error(err);
+    err = ioctl(fd, WLIOC_SETPRLEN, config->preamble);
+    config_error(err);
+// #endif /* defined(CONFIG_LPWAN_RN2XX3) */
+
+    return err;
+}
 
 void* collection_main(void* args){
     #ifdef CONFIG_INSPACE_TRACKER_RADIO_MOCK 
@@ -34,7 +78,58 @@ void* collection_main(void* args){
         }
 
         fclose(telem_file);
-        ininfo("Telem file closed");
-        pthread_exit(NULL);
     #endif
+
+    #ifdef CONFIG_INSPACE_TRACKER_RADIO
+    int err;
+
+    int radio_fd = open(CONFIG_INSPACE_TRACKER_RADIO_PATH, O_RDWR);
+    if (radio_fd < 0) {
+        err = errno;
+        inerr("Error getting radio handle: %d\n", err);
+        goto err_cleanup;
+    }
+
+    /* TODO: maybe add this to the config */
+    struct radio_options config = {
+        .sync = 0x43,
+        .freq = 433050000,
+        .txpwr = 15,
+        .bw = 125,
+        .preamble = 6,
+        .spread = 7,
+        .cr = RN2XX3_CR_4_5,
+        .crc = true,
+        .iqi = false,
+    };
+
+    err = configure_radio(radio_fd, &config);
+    if (err) {
+        inerr("Error configuring radio: %d\n", err);
+        /* Error will have been reported in configure_rn2483 where we can say which
+         * config failed in particular */
+        goto err_cleanup;
+    }
+
+    uint8_t buffer[PACKET_MAX_SIZE];
+    ssize_t b_read;
+
+    for(;;){
+        b_read = read(radio_fd, buffer, sizeof(buffer));
+        if (b_read < 0)
+        {
+            err = errno;
+            inerr("Error receiving from radio: %d\n", err);
+            goto err_cleanup;
+        }
+        printf("%s\n", buffer);
+    }
+
+err_cleanup:
+        if (radio_fd != -1) {
+            close(radio_fd);
+        }
+    #endif
+    
+    pthread_exit(NULL);
 }
--- tracker/src/collection/radio.h
+++ tracker/src/collection/radio.h
@@ -2,6 +2,7 @@
 #define _INSPACE_TRACKER_RADIO_H_
 
 #include "../packets/packet.h"
+#include <stdbool.h>
 
 typedef struct {
     uint16_t timestamp;
@@ -11,6 +12,21 @@ typedef struct {
     struct accel_blk_t accel;
     struct coord_blk_t coord;
 } parsed_packet;
+
+#ifdef CONFIG_LPWAN_RN2XX3
+#include <nuttx/wireless/lpwan/rn2xx3.h>
+struct radio_options {
+    uint64_t sync;       /* Sync word */
+    uint32_t freq;       /* Frequency, Hz */
+    int32_t txpwr;       /* Transmit power, dBm */
+    uint32_t bw;         /* Bandwidth, kHz */
+    uint16_t preamble;   /* Preamble length */
+    uint8_t spread;      /* Spread factor */
+    enum rn2xx3_cr_e cr; /* Coding rate */
+    bool crc;            /* CRC enabled */
+    bool iqi;            /* IQI enabled */
+};
+#endif
 
 parsed_packet parse_packet(char* buffer);
 
