98814b8ed1045bef36c01eb2f2ed9d93ffe1624e
Daniel Byshkin <daniel.byshkin@gmail.com>
2025-11-11 22:04:15 -0500

    Removing el blasto radio mocking, packet formatting changed

--- tracker/src/collection/collection.c
+++ tracker/src/collection/collection.c
@@ -5,7 +5,6 @@
 #include "../packets/packet.h"
 #include <pthread.h>
 #include "collection.h"
-#include "mocking/el_blasto.h"
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <uORB/uORB.h>
@@ -162,34 +161,6 @@ void* collection_main(void* args){
             pthread_exit(NULL);
         }
     }
-
-    #ifdef CONFIG_INSPACE_TRACKER_RADIO_MOCK 
-        ininfo("Mocking radio data\n");
-
-        FILE* telem_file = fmemopen(EL_BLASTO_RAW_HEX, sizeof(EL_BLASTO_RAW_HEX), "r");
-        if(telem_file == NULL){
-            inerr("Failed to open telem file: %s\n", strerror(errno));
-            pthread_exit(NULL);
-        }
-
-        char buffer[PACKET_MAX_SIZE];
-        char line[PACKET_MAX_SIZE * 2];
-        while(fgets(line, sizeof(line), telem_file) != NULL){
-            size_t byte_count = 0;
-
-            /* TODO: handle odd len hex strings*/
-            for (size_t i = 0; i < strlen(line); i += 2) {
-                if (line[i] == '\n' || line[i + 1] == '\n') {
-                    break;
-                }
-                
-                sscanf(&line[i], "%2hhx", (unsigned char*)&buffer[byte_count++]);
-            }
-            parse_packet(buffer, byte_count, orb_fds_out);
-        }
-
-        fclose(telem_file);
-    #endif
 
     #ifdef CONFIG_INSPACE_TRACKER_RADIO
     int err;
